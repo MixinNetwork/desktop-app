d12f13fda0de61db8d78c9f911da055584a1e8d8
Crossle Song <crosslesong@gmail.com>
2019-03-18 20:51:34 +0800

    Fix contact card

--- src/components/ContactItem.vue
+++ src/components/ContactItem.vue
@@ -4,18 +4,29 @@
       <Avatar id="avatar" :user="user"/>
       <div class="content">
         <span class="name">{{message.sharedUserFullName}}</span>
-        <span class="number">{{message.sharedUserIdentityNumber}}</span>
+        <div class="bottom">
+          <span class="number">{{message.sharedUserIdentityNumber}}</span>
+          <span class="time">
+            {{message.lt}}
+            <ICSending
+              v-if="message.userId === me.user_id && (message.status === MessageStatus.SENDING || message.status === MessageStatus.PENDING)"
+              class="icon"
+            />
+            <ICSend
+              v-else-if="message.userId === me.user_id && message.status === MessageStatus.SENT"
+              class="icon"
+            />
+            <ICRead
+              v-else-if="message.userId === me.user_id && message.status === MessageStatus.DELIVERED"
+              class="icon wait"
+            />
+            <ICRead
+              v-else-if="message.userId === me.user_id && message.status === MessageStatus.READ"
+              class="icon"
+            />
+          </span>
+        </div>
       </div>
-      <span class="time" v-if="message.userId === me.user_id">
-        {{message.lt}}
-        <ICSending
-          v-if="message.status === MessageStatus.SENDING || message.status === MessageStatus.PENDING"
-          class="icon"
-        />
-        <ICSend v-else-if="message.status === MessageStatus.SENT" class="icon"/>
-        <ICRead v-else-if="message.status === MessageStatus.DELIVERED" class="icon wait"/>
-        <ICRead v-else-if="message.status === MessageStatus.READ" class="icon"/>
-      </span>
     </div>
   </div>
 </template>
@@ -61,43 +72,56 @@ export default {
   padding-left: 0.8rem;
   padding-right: 0.8rem;
   .contact {
-    padding-left: 12px;
-    padding-right: 0.2rem;
-    padding-top: 12px;
-    padding-bottom: 12px;
+    padding: 12px;
     background: white;
-    border-radius: 0.4rem;
     display: flex;
     flex-direction: row;
-    max-width: 12rem;
+    align-content: center;
+    width: 12rem;
+    border-radius: 0.4rem;
     box-shadow: 1px 1px 1px #33333333;
     #avatar {
-      margin-right: 16px;
+      width: 42px;
+      height: 42px;
+      margin-right: 12px;
     }
     .content {
       display: flex;
+      flex: 1;
       flex-direction: column;
-      justify-content: space-around;
-      margin-right: 16px;
       text-align: start;
+      overflow: hidden;
       .name {
         font-size: 1rem;
+        overflow: hidden;
+        white-space: nowrap;
+        text-overflow: ellipsis;
       }
-      .number {
-        color: #88888888;
-        font-size: 0.8rem;
-      }
-    }
-    .time {
-      color: #8799a5;
-      display: flex;
-      float: right;
-      font-size: 0.75rem;
-      bottom: 0.3rem;
-      right: 0.2rem;
-      align-items: flex-end;
-      .icon {
-        padding-left: 0.2rem;
+      .bottom {
+        display: flex;
+        justify-content: space-between;
+        .number {
+          color: #88888888;
+          font-size: 0.8rem;
+          margin-top: 6px;
+        }
+        .time {
+          color: #8799a5;
+          display: flex;
+          float: right;
+          font-size: 0.75rem;
+          bottom: 0.3rem;
+          right: 0.2rem;
+          align-items: flex-end;
+          .icon {
+            padding-left: 0.2rem;
+          }
+          .wait {
+            path {
+              fill: #859479;
+            }
+          }
+        }
       }
     }
   }

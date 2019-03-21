54df44be696c5440c7dda95f812a655531ed45cc
Crossle Song <crosslesong@gmail.com>
2019-03-21 14:42:25 +0800

    Paste image event

--- src/components/ChatContainer.vue
+++ src/components/ChatContainer.vue
@@ -183,6 +183,22 @@ export default {
     this.$refs.box.addEventListener('compositionend', function() {
       self.inputFlag = false
     })
+    document.onpaste = function(e) {
+      const items = (event.clipboardData || event.originalEvent.clipboardData).items
+      let blob = null
+      for (let i = 0; i < items.length; i++) {
+        if (items[i].type.indexOf('image') === 0) {
+          blob = items[i].getAsFile()
+        }
+      }
+      if (blob !== null) {
+        let reader = new FileReader()
+        reader.onload = function(event) {
+          console.log(event.target.result)
+        }
+        reader.readAsDataURL(blob)
+      }
+    }
   },
   lastEnter: null,
   methods: {

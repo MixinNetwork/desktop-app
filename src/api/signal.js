78dac7a6bc31316ce94d5663902a14faf32a3d3d
SeniorZhai <developer.zhaitao@gmail.com>
2019-11-26 14:00:10 +0800

    Check signal key

--- src/api/signal.js
+++ src/api/signal.js
@@ -3,5 +3,8 @@ import api from '@/api/base.js'
 export default {
   postSignalKeys(body) {
     return api.post('/signal/keys', body)
+  },
+  getSignalKeyCount() {
+    return api.get('/signal/keys/count')
   }
 }

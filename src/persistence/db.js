e3cdd76c7f444ec08567afe7688bae002647b399
jim <jimchen@mixin.one>
2019-12-31 16:04:26 +0800

    Drop apps table column

--- src/persistence/db.js
+++ src/persistence/db.js
@@ -22,6 +22,14 @@ setTimeout(() => {
         mixinDb.exec('DROP TABLE IF EXISTS snapshots')
       }
       if (row.user_version < 2) {
+        mixinDb.exec(
+          'CREATE TABLE `apps_backup` (`app_id` TEXT NOT NULL, `app_number` TEXT NOT NULL, `home_uri` TEXT NOT NULL, `redirect_uri` TEXT NOT NULL, `name` TEXT NOT NULL, `icon_url` TEXT NOT NULL, `description` TEXT NOT NULL, `capabilites` TEXT,  `creator_id` TEXT NOT NULL, PRIMARY KEY(`app_id`))'
+        )
+        mixinDb.exec(
+          'INSERT INTO apps_backup SELECT app_id, app_number, home_uri, redirect_uri, name, icon_url, description, capabilites, creator_id FROM apps'
+        )
+        mixinDb.exec('DROP TABLE apps')
+        mixinDb.exec('ALTER TABLE apps_backup RENAME TO apps')
         mixinDb.exec('ALTER TABLE users ADD COLUMN biography TEXT')
       }
       mixinDb.exec('DROP TRIGGER IF EXISTS conversation_unseen_message_count_update')

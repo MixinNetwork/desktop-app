1957dafab1bdad44da66c7d18c6ec638fd450796
Crossle Song <crosslesong@gmail.com>
2020-01-20 20:30:18 +0800

    Add SNAPSHOTS READ scope

--- src/utils/util.js
+++ src/utils/util.js
@@ -48,7 +48,7 @@ export function getToken(method, uri, data) {
     const sid = account.session_id
     const m = method.toUpperCase()
     const scp =
-      'PROFILE:READ PROFILE:WRITE PHONE:READ PHONE:WRITE CONTACTS:READ CONTACTS:WRITE MESSAGES:READ MESSAGES:WRITE ASSETS:READ'
+      'PROFILE:READ PROFILE:WRITE PHONE:READ PHONE:WRITE CONTACTS:READ CONTACTS:WRITE MESSAGES:READ MESSAGES:WRITE ASSETS:READ SNAPSHOTS:READ'
     token = new Bot().signAuthenticationToken(uid, sid, privateKey, m, uri, data, scp)
   }
   return token

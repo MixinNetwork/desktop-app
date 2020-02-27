4d8dd737cb6ad3e1e43dfbcee73a46d4853e0634
SeniorZhai <developer.zhaitao@gmail.com>
2020-02-27 16:40:23 +0800

    Fix typo

--- src/dao/message_mention_dao.ts
+++ src/dao/message_mention_dao.ts
@@ -1,9 +1,9 @@
 import db from '@/persistence/db'
 
-class MessageMetionDao {
+class MessageMentionDao {
   getMentionData(messageId: any) {
     return db.prepare('SELECT mentions FROM message_mentions WHERE message_id = ?').get(messageId)
   }
 }
 
-export default new MessageMetionDao()
+export default new MessageMentionDao()

3436a94cea34cc959fea01a42d2d5f276a111e1f
jim <jimchen@mixin.one>
2020-03-25 17:08:29 +0800

    Get mixinContext in advance

--- src/utils/browser.ts
+++ src/utils/browser.ts
@@ -1,50 +1,30 @@
-import { remote, nativeTheme } from 'electron'
+import { remote } from 'electron'
 let { BrowserWindow } = remote
 
 let browser: any = null
-const appVersion = remote.app.getVersion()
+
+const path = require('path')
+// @ts-ignore
+const preloadFile = path.join(__static, 'preload/browser_inject.js')
 
 export default {
   loadURL(url: any, conversationId: string) {
-    if (!browser) {
-      browser = new BrowserWindow({
-        resizable: false,
-        minimizable: false,
-        fullscreenable: false,
-        show: false,
-        webPreferences: {
-          sandbox: true
-        }
-      })
-
-      let appearance = 'light'
-      if (nativeTheme && nativeTheme.shouldUseDarkColors) {
-        appearance = 'dark'
-      }
-
-      browser.webContents.on('did-finish-load', () => {
-        const mixinContext = {
-          conversation_id: conversationId,
-          immersive: false,
-          app_version: appVersion,
-          appearance
-        }
-        browser.webContents.executeJavaScript(
-          `window.MixinContext = {
-            getContext: function() {
-              return '${JSON.stringify(mixinContext)}'
-            }
-          }`,
-          true
-        )
-      })
-      browser.on('closed', () => {
-        browser = null
-      })
-      browser.on('ready-to-show', () => {
-        browser.show()
-      })
+    if (conversationId) {
+      remote.app.name = conversationId
     }
+    browser = null
+    browser = new BrowserWindow({
+      resizable: false,
+      minimizable: false,
+      fullscreenable: false,
+      webPreferences: {
+        sandbox: true,
+        preload: preloadFile
+      }
+    })
+    browser.on('closed', () => {
+      browser = null
+    })
     browser.loadURL(url)
     browser.moveTop()
   }

8136f8e5d19258fa12dc1c5a93f148c24267d964
jim <jimchen@mixin.one>
2020-05-20 15:18:28 +0800

    Fix reconnect condition

--- src/blaze/blaze.js
+++ src/blaze/blaze.js
@@ -27,7 +27,7 @@ class Blaze {
     clearInterval(this.connectInterval)
     this.connectInterval = setInterval(() => {
       this.connecting = false
-      if (store.state.linkStatus !== LinkStatus.CONNECTED || (this.ws && this.ws.readyState !== WebSocket.OPEN)) {
+      if (!this.ws || (this.ws && this.ws.readyState !== WebSocket.OPEN)) {
         console.log('--- connect interval --', this.ws && this.ws.readyState, store.state.linkStatus)
         if (this.ws) {
           this.ws.close(1000, 'Normal close')

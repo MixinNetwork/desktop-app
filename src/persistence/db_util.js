5c0ebf197aab628a5914d07f35d374c8941c3bd7
jim <jimchen@mixin.one>
2020-05-21 16:06:41 +0800

    Fix signal.db3 path err

--- src/persistence/db_util.js
+++ src/persistence/db_util.js
@@ -5,7 +5,7 @@ import { clearAllTables as clearSignal } from './signal_db'
 import { clearKeyTable } from './db'
 import store from '@/store/store'
 
-export function getDbPath() {
+export function getDbPath(isOld) {
   const isDevelopment = process.env.NODE_ENV !== 'production'
   let dir = remote.app.getPath('userData')
   let identityNumber = ''
@@ -13,7 +13,7 @@ export function getDbPath() {
     const user = JSON.parse(localStorage.account)
     identityNumber = user.identity_number
   }
-  if (!isDevelopment && identityNumber) {
+  if (!isDevelopment && identityNumber && !isOld) {
     const newDir = path.join(dir, identityNumber)
     const dbPath = path.join(newDir, `mixin.db3`)
     if (fs.existsSync(dbPath)) {
@@ -26,10 +26,10 @@ export function getDbPath() {
 export async function dbMigration(identityNumber) {
   const isDevelopment = process.env.NODE_ENV !== 'production'
   if (isDevelopment) return
-  const src = path.join(getDbPath(), 'mixin.db3')
+  const src = path.join(getDbPath(true), 'mixin.db3')
   const dist = path.join(remote.app.getPath('userData'), `${identityNumber}/mixin.db3`)
   fs.writeFileSync(dist, fs.readFileSync(src))
-  const signalSrc = path.join(getDbPath(), 'signal.db3')
+  const signalSrc = path.join(getDbPath(true), 'signal.db3')
   const signalDist = path.join(remote.app.getPath('userData'), `${identityNumber}/signal.db3`)
   fs.writeFileSync(signalDist, fs.readFileSync(signalSrc))
 }

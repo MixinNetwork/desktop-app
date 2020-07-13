ade5b121cd6561949a041ae62a322d48ea5be7bb
jim <jimchen@mixin.one>
2020-07-13 18:48:07 +0800

    Slight fix

--- src/utils/attachment_util.ts
+++ src/utils/attachment_util.ts
@@ -191,11 +191,13 @@ function getMediaNewDir(category: string, identityNumber: string, conversationId
   return dir
 }
 
-export async function downloadSticker(stickerId: string, createdAt: string) {
+export async function downloadSticker(stickerId: string, createdAt?: string) {
   const response = await stickerApi.getStickerById(stickerId)
   if (response.data.data) {
     const resData = response.data.data
-    resData.created_at = createdAt
+    if (createdAt) {
+      resData.created_at = createdAt
+    }
     stickerDao.insertUpdate(resData)
     const data: any = await getAttachment(resData.asset_url, stickerId)
     const identityNumber = getIdentityNumber()

01e758b4a1b84663d9d951834b74a9e3703bb85c
jim <jimchen@mixin.one>
2020-08-17 23:16:41 +0800

    Refine code

--- src/blaze/blaze.js
+++ src/blaze/blaze.js
@@ -19,7 +19,6 @@ class Blaze {
     this.wsBaseUrl = API_URL.WS[0]
     this.account = getAccount()
     this.TIMEOUT = 'Time out'
-    this.CONNECTING = 'Connecting'
     this.timeoutTimer = null
     this.pingInterval = null
     this.wsInitialLock = false
@@ -197,7 +196,7 @@ class Blaze {
   sendMessagePromise(message) {
     if (this.ws && this.ws.readyState === WebSocket.CONNECTING) {
       return new Promise((resolve, reject) => {
-        reject(this.CONNECTING)
+        reject(this.TIMEOUT)
       })
     }
     return new Promise((resolve, reject) => {

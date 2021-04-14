ed725b73780ba1f404c1b6fdd8494c34b0ca765d
LiJinlei <grenlight@icloud.com>
2021-04-14 16:42:12 +0800

    Keep new window always displayed on the screen where the main window is located

--- src/utils/browser.ts
+++ src/utils/browser.ts
@@ -1,4 +1,5 @@
 import { remote, ipcRenderer } from 'electron'
+import windowStateKeeper from 'electron-window-state'
 let { BrowserWindow } = remote
 
 let browser: any = null
@@ -13,7 +14,12 @@ export default {
       ipcRenderer.send('currentConversationId', conversationId)
     }
     if (!browser) {
+      // Regardless of whether the main window is on the main screen or the external screen,
+      // the new window is always displayed on the screen where the main window is located.
+      let mainWindowState = windowStateKeeper({})
       browser = new BrowserWindow({
+        x: mainWindowState.x + (mainWindowState.width - 800) / 2,
+        y: mainWindowState.y + (mainWindowState.height - 600) / 2,
         resizable: false,
         minimizable: false,
         fullscreenable: true,
